6710195b7b33b6c971a5477b7c1a58a779de4cc2
Reyhan Mohammed <reyhan.mohammed@atmecs.com>
2021-12-11 22:16:12 +0530

    Learned React state

---
+++ src/Components/Details.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Details extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>Details of mazen</h1>
+            <h7>Details of mazen</h7>
+            <h7>age of mazen</h7>
+            <h7>age of mazen</h7>
+        </div>
+    );
+  }
+}
+
+export default Details;
